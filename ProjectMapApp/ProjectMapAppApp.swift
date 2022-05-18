e92f7f41ec6d9a15f231a6bd42b7b3a485a5a0f2
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-05-18 20:12:22 +0200

    Initial Commit

---
+++ ProjectMapApp/ProjectMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectMapAppApp.swift
+//  ProjectMapApp
+//
+//  Created by Łukasz Gołojuch on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
